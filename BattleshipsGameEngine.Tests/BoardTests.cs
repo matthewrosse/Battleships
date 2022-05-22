f0c5df0cf97e5dadebc6919d81331e0bb108c13c
Mateusz Różański <rozanski.mateusz00@gmail.com>
2022-05-22 03:53:31 +0200

    Added tests for ship and board

---
+++ BattleshipsGameEngine.Tests/BoardTests.cs
@@ -0,0 +1,80 @@
+﻿using BattleshipsGameEngine.Entities;
+using BattleshipsGameEngine.Enums;
+
+namespace BattleshipsGameEngine.Tests;
+
+public class BoardTests
+{
+    [Fact]
+    public void PlaceShip_Two_Horizontally_Should_Return_True()
+    {
+        Board board = new Board(10);
+        Ship firstShip = new Ship(4, 7, 4, Direction.Horizontal);
+        Ship secondShip = new Ship(1, 3, 3, Direction.Horizontal);
+        board.PlaceShip(firstShip);
+        
+        Assert.True(board.PlaceShip(secondShip));
+    }
+
+    [Fact]
+    public void PlaceShip_One_Horizontally_One_Vertically_Should_Return_False()
+    {
+        Board board = new Board(10);
+        
+        Ship firstShip = new Ship(3, 1, 3, Direction.Horizontal);
+        Ship secondShip = new Ship(4, 1, 3, Direction.Vertical);
+        board.PlaceShip(firstShip);
+        
+        Assert.False(board.PlaceShip(secondShip));
+    }
+    
+    [Fact]
+    public void PlaceShip_Two_Vertically_Should_Return_False()
+    {
+        Board board = new Board(10);
+        Ship firstShip = new Ship(0, 0, 4, Direction.Vertical);
+        Ship secondShip = new Ship(0, 1, 4, Direction.Vertical);
+        board.PlaceShip(firstShip);
+
+        Assert.False(board.PlaceShip(secondShip));
+    }
+
+    [Fact]
+    public void PlaceShip_Two_Horizontally_Should_Return_False()
+    {
+        Board board = new Board(10);
+        Ship firstShip = new Ship(0, 0, 4, Direction.Horizontal);
+        Ship secondShip = new Ship(1, 0, 4, Direction.Horizontal);
+        board.PlaceShip(firstShip);
+
+        Assert.False(board.PlaceShip(secondShip));
+    }
+    
+    [Fact]
+    public void PlaceShip_Horizontally_Outside_Array_Should_Return_False()
+    {
+        Board board = new Board(10);
+        Ship firstShip = new Ship(0, 8, 4, Direction.Horizontal);
+
+        Assert.False(board.PlaceShip(firstShip));
+    }
+    
+    [Fact]
+    public void PlaceShip_Vertically_Outside_Array_Should_Return_False()
+    {
+        Board board = new Board(10);
+        Ship firstShip = new Ship(8, 0, 4, Direction.Horizontal);
+
+        Assert.False(board.PlaceShip(firstShip));
+    }
+
+    [Fact]
+    public void PlaceShip_One_Vertically_One_Horizontally_Should_Return_False()
+    {
+        Board board = new Board(10);
+        Ship firstShip = new Ship(3, 1, 3, Direction.Horizontal);
+        Ship secondShip = new Ship(2, 0, 3, Direction.Vertical);
+        board.PlaceShip(firstShip);
+        Assert.False(board.PlaceShip(secondShip));
+    }
+}
---
+++ BattleshipsGameEngine.Tests/ShipTests.cs
@@ -0,0 +1,50 @@
+﻿using BattleshipsGameEngine.Entities;
+using BattleshipsGameEngine.Enums;
+
+namespace BattleshipsGameEngine.Tests;
+
+public class ShipTests
+{
+    [Fact]
+    public void Creating_Ship_Should_Return_All_True_Masts()
+    {
+        Ship ship = new Ship(1, 2, 6, Direction.Horizontal);
+            bool isAnyMastSunk = false;
+            foreach (var mast in ship.Masts)
+            {
+                if (!mast)
+                {
+                    isAnyMastSunk = true;
+                    break;
+                }
+            }
+            Assert.False(isAnyMastSunk);
+    }
+
+    [Fact]
+    public void IsSunk_Should_Return_True()
+    {
+        Ship ship = new Ship(1, 2, 6, Direction.Horizontal);
+        for (int i = 0; i < ship.Masts.Length; i++)
+        {
+            ship.Masts[i] = false;
+        }
+
+        Assert.True(ship.IsSunk());
+    }
+    [Fact]
+    public void IsSunk_One_Mast_Hit_Should_Return_False()
+    {
+        Ship ship = new Ship(1, 2, 6, Direction.Horizontal);
+        ship.Masts[0] = false;
+
+        Assert.False(ship.IsSunk());
+    }
+    [Fact]
+    public void IsSunk_Healthy_Ship_Should_Return_False()
+    {
+        Ship ship = new Ship(1, 2, 6, Direction.Horizontal);
+        Assert.False(ship.IsSunk());
+    }
+    
+}
